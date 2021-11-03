e633acab83e2d64f7a1b95202260e8e0b3ab8e8d
damien-simplon <damien.bellec12@gmail.com>
2021-11-03 13:28:36 +0100

    suppresion du fichier script, liaison avec la bdd

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # mise-en-place-d-une-API-d-authentification
 npm i express
 npm i express-jwt
+npm i mysql
--- js/server.js
+++ js/server.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const mysql = require('mysql');
+const db = mysql.createConnection({
+    host: "localhost",
+    user : "root",
+    password : "",
+    database : "miseenplaceapi"
+})
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connecté à la base de données MySQL!");
+  });
+
 app.listen(port, function(err){
     if(err) console.log(err);
     console.log("Server listening on port : " + port);
