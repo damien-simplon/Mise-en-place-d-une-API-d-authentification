926769b0bb17a1fe5f0f662e3b13b96e87d020d9
damien-simplon <damien.bellec12@gmail.com>
2021-11-16 10:47:21 +0100

    test du token effectué

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.onload = function () {
 		connexion.addEventListener('submit', (e) => {
             e.preventDefault();
             const form = new FormData(connexion);
-            fetch('/api/login', {
+            fetch('localhost:3000/api/users', {
                 method: 'POST',
                 body: form
             })
