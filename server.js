5f58ba21279a57b37f4d15b16910d8a9601c1d03
damien-simplon <damien.bellec12@gmail.com>
2021-11-22 09:24:52 +0100

    changement de la méthode pour cors

--- server.js
+++ server.js
@@ -10,26 +10,7 @@ const registerLoginRoutes = require('./routes/registerLoginRoutes');
 
 // express url encoded
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({origin: '*'}));
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://damien-api-jwt.herokuapp.com/');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+app.use(cors({origin: "*"}));
 
 const start = async () => {
 	try {
@@ -45,8 +26,6 @@ const start = async () => {
 
 start();
 
-app.options('/api/users/', cors());
 app.use('/api/users/', userRoutes);
 
-app.options('/api/', cors());
 app.use('/api/', registerLoginRoutes);
